1ad92b1f063a7862a852e303ceb8aa26f65f026a
Jos Visser <goleztrol@hotmail.com>
2022-05-25 14:09:59 +0200

    Stash before rebasing. Add rebase on main

--- Git/rebase_on_main.bat
+++ Git/rebase_on_main.bat
@@ -3,8 +3,8 @@ setlocal
 set gitpath=%1
 if "%gitpath%"=="" set gitpath=git
 
-%gitpath% fetch upstream master:master
-%gitpath% push origin master
+%gitpath% fetch upstream main:main
+%gitpath% push origin main
 %gitpath% stash push
-%gitpath% rebase master
+%gitpath% rebase main
 %gitpath% stash pop
