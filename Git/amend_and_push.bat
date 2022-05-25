75109d2e9b99cb396065ddf3962f7d095a08c3bc
Jos Visser <goleztrol@hotmail.com>
2022-05-25 14:09:21 +0200

    Git scripts accept optional git path

--- Git/amend_and_push.bat
+++ Git/amend_and_push.bat
@@ -1,2 +1,7 @@
-git commit --amend
-git push --force
+@echo off
+setlocal
+set gitpath=%1
+if "%gitpath%"=="" set gitpath=git
+
+%gitpath% commit --amend --no-edit
+%gitpath% push --force
--- Git/rebase_on_master.bat
+++ Git/rebase_on_master.bat
@@ -1,3 +1,8 @@
-git fetch upstream master:master
-git push origin master
-git rebase master
+@echo off
+setlocal
+set gitpath=%1
+if "%gitpath%"=="" set gitpath=git
+
+%gitpath% fetch upstream master:master
+%gitpath% push origin master
+%gitpath% rebase master
