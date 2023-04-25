760837cf33b8d44e8646c7c3c3396dab2ea3dd63
Jos Visser <goleztrol@hotmail.com>
2023-04-25 10:59:27 +0200

    Rebase figures out name of main branch
    No separate rebase on master anymore.

--- Git/rebase_on_main.bat
+++ Git/rebase_on_main.bat
@@ -1,8 +1,20 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 set gitpath=%1
 if "%gitpath%"=="" set gitpath=git
 
-%gitpath% fetch upstream main:main
-%gitpath% push origin main
-%gitpath% rebase main --autostash
+set main=
+for /f "tokens=* USEBACKQ" %%a in (`%gitpath% remote show upstream ^| FindStr /L "HEAD branch:"`) do (
+  set mainline=%%a
+  set mainbit=!mainline:~13!
+  REM echo "!mainline!"--"!mainbit!"
+  REM 'Remote branch' also matches. Just keeping the first line seems to do the trick.
+  if "!main!"=="" set main=!mainbit!
+)
+
+echo Main branch: %main%
+
+:ok
+%gitpath% fetch upstream %main%:%main%
+%gitpath% push origin %main%
+%gitpath% rebase %main% --autostash
--- Git/rebase_on_master.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-set gitpath=%1
-if "%gitpath%"=="" set gitpath=git
-
-%gitpath% fetch upstream master:master
-%gitpath% push origin master
-%gitpath% rebase master --autostash
